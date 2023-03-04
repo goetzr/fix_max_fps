7672ce2b0b2acb8262faa57ad52e9b3effd713a0
Russ Goetz <russgoetz@gmail.com>
2023-03-04 16:11:27 -0500

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::{Path, PathBuf};
-use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::fs::{OpenOptions};
+use std::io::{BufReader, BufRead, BufWriter, Write};
 
 mod error;
 
@@ -11,6 +11,7 @@ const MINECRAFT_DIR: &'static str = ".minecraft";
 const OPTIONS_FILE_NAME: &'static str = "options.txt";
 const OPTIONS_SEPARATOR: &'static str = ":";
 const MAX_FPS_OPTION: &'static str = "maxFps";
+const GOOD_VALUE: &'static str = "120";
 
 fn main() {
     if let Err(e) = try_main() {
@@ -22,7 +23,16 @@ fn try_main() -> anyhow::Result<()> {
     let options_file_path = get_options_file_path()?;
     println!("INFO: Options file found at {}", options_file_path.display());
 
-    let options = read_options(&options_file_path)?;
+    let mut options = read_options(&options_file_path)?;
+    if is_max_fps_zero(options.as_slice())? {
+        fix_max_fps(&mut options);
+        write_options(options.as_slice(), &options_file_path)?;
+        println!("INFO: {} option fixed!", MAX_FPS_OPTION);
+    } else {
+        println!("INFO: {} option is OK!", MAX_FPS_OPTION);
+    }
+
+    unsafe { libc::getchar() };
     
     Ok(())
 }
@@ -48,7 +58,7 @@ struct Option {
 }
 
 fn read_options(path: &Path) -> Result<Vec<Option>> {
-    let file = File::open(path).map_err(|e| Error::ReadOptionsFile(e))?;
+    let file = OpenOptions::new().read(true).open(path).map_err(|e| Error::ReadOptionsFile(e))?;
     let reader = BufReader::new(file);
     let mut options = Vec::new();
     for (line_num, line) in reader.lines().enumerate() {
@@ -60,4 +70,22 @@ fn read_options(path: &Path) -> Result<Vec<Option>> {
         options.push(Option { key: parts[0].to_string(), value: parts[1].to_string() });
     }
     Ok(options)
+}
+
+fn is_max_fps_zero(options: &[Option]) -> Result<bool> {
+    Ok(options.iter().find(|op| op.key == MAX_FPS_OPTION).ok_or(Error::MaxFpsOptionMissing)?.value == "0")
+}
+
+fn fix_max_fps(options: &mut Vec<Option>) {
+    options.iter_mut().find(|op| op.key == MAX_FPS_OPTION).expect("option missing").value = GOOD_VALUE.to_string();
+}
+
+fn write_options(options: &[Option], path: &Path) -> Result<()> {
+    let file = OpenOptions::new().write(true).open(path).map_err(|e| Error::WriteOptionsFile(e))?;
+    let mut writer = BufWriter::new(file);
+    for option in options.iter() {
+        writer.write_all(format!("{}{}{}\n", option.key, OPTIONS_SEPARATOR, option.value).as_bytes()).map_err(|e| Error::WriteOptionsFile(e))?;
+    }
+    writer.flush().map_err(|e| Error::WriteOptionsFile(e))?;
+    Ok(())
 }
